0c3303e8d77b8ea2e2efa0bbbb6b854a415580f5
CHEF-KOCH <CHEF-KOCH@Protonmail.com>
2018-05-29 15:03:41 +0200

    test
    Signed-off-by: CHEF-KOCH <CHEF-KOCH@Protonmail.com>

--- banner.js
+++ banner.js
@@ -8,8 +8,8 @@ const date = `${dateObj.getFullYear()}-${padStart(
   dateObj.getMonth() + 1
 )}-${padStart(dateObj.getDate())}`;
 const banner = `/*!
- * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
- * Copyright 2013-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
+ * CK's Theme ${pkg.version} by ${pkg.author}
+ * Copyright 2017-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
  * Licensed under ${pkg.license}
  */
 `;
